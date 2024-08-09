821d5c17e6d435d421f7232619d9ca309c4e16c7
Deodate <ajadeo04@gmail.com>
2024-08-09 17:50:15 +0200

    Progress

--- controllers/citizen.controller.js
+++ controllers/citizen.controller.js
@@ -1,11 +1,17 @@
-const express = require('express'),
-router = express.Router()
+const express = require('express');
+const router = express.Router();
+const service = require('../service/citizen.service');
 
-const service = require('../service/citizen.service')
+// Route to get all citizens
+router.get('/', async (req, res) => {
+    const citizens = await service.getAllCitizen();
+    res.send(citizens);
+});
 
-router.get('/', async(req, res) => {
-    const citizen = await service.getAllCitizen()
-    res.send(citizen)
-})
+// Route to get a specific citizen by ID
+router.get('/:id', async (req, res) => {
+    const citizen = await service.getCitizenById(req.params.id);
+    res.send(citizen);
+});
 
 module.exports = router;
--- service/citizen.service.js
+++ service/citizen.service.js
@@ -1,7 +1,13 @@
-const db = require('../db')
+const db = require('../db');
 
+// Function to get all citizens
 module.exports.getAllCitizen = async () => {
-    const [rows] = await db.query("SELECTs * FROM citizen")
-    .catch(err => console.log(err))
+    const [rows] = await db.query("SELECT * FROM citizen");
     return rows;
-}
+};
+
+// Function to get a citizen by ID
+module.exports.getCitizenById = async (id) => {
+    const [rows] = await db.query("SELECT * FROM citizen WHERE id = $1", [id]);
+    return rows[0]; // Assuming id is unique, return the first (and only) row
+};
